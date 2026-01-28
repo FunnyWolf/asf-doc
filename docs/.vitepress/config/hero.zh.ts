b662545a7a5082092e858fdcd5e33ddbc65a1f1e
rootkit <yu5890681@gmail.com>
2026-01-28 13:57:53 +0800

    update version 0.2.0

--- docs/.vitepress/config/hero.zh.ts
+++ docs/.vitepress/config/hero.zh.ts
@@ -1,7 +1,7 @@
 export const heroConfig = {
     name: 'Agentic SOC Platform',
     tagline: 'AI 智能体驱动的自动化,赋予您的 SOC 洞察先机,从容应对威胁',
-    image: "/hero/zh/img_20.png",
+    image: "/hero/zh/img_20.png?v=1",
     actions: [
         {text: '开发文档', link: '/zh/asf/Development/environment_setup', theme: 'brand'},
         {text: '功能介绍', link: '/zh/asf/Introduction/what_is_asf/', theme: 'alt'},
